f4c9c8ee3b3de6a4ab8698d3e3e050b2e15e2fac
serhii <serhii.yakymenko9@gmail.com>
2025-06-30 17:29:50 +0300

    add ivicorn in main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from os import environ as env
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def index():
     return {"details": "Hello, wo"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
 
 
