4608b26b0a854519bd4dfe3c989d7bba5f35a034
serhii <serhii.yakymenko9@gmail.com>
2025-06-30 19:11:54 +0300

    Change text

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI
 from os import environ as env
-import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"details": "Hello, wo"}
+    return {"details": "Hello, world"}
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
 
 
 
